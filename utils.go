30ab7fdf091930feddc8c778c55aa63e86bc915a
Minh Vuong <38932626+vuongxuongminh@users.noreply.github.com>
2022-05-04 23:28:18 +0700

    feat: add utility to normalize graphql request (#29)

--- utils.go
+++ utils.go
@@ -3,9 +3,12 @@ package gbox
 import (
 	"bytes"
 	"net/http"
+	"strings"
 	"sync"
 
+	"github.com/jensneuse/graphql-go-tools/pkg/astparser"
 	"github.com/jensneuse/graphql-go-tools/pkg/graphql"
+	"github.com/jensneuse/graphql-go-tools/pkg/operationreport"
 )
 
 var bufferPool = sync.Pool{
@@ -21,6 +24,37 @@ func writeResponseErrors(errors error, w http.ResponseWriter) error {
 	if _, err := gqlErrors.WriteResponse(w); err != nil {
 		return err
 	}
+
+	return nil
+}
+
+func normalizeGraphqlRequest(schema *graphql.Schema, gqlRequest *graphql.Request) error {
+	if result, _ := gqlRequest.Normalize(schema); !result.Successful {
+		return result.Errors
+	}
+
+	operation, _ := astparser.ParseGraphqlDocumentString(gqlRequest.Query)
+	numOfOperations := operation.NumOfOperationDefinitions()
+	operationName := strings.TrimSpace(gqlRequest.OperationName)
+	report := &operationreport.Report{}
+
+	if operationName == "" && numOfOperations > 1 {
+		report.AddExternalError(operationreport.ErrRequiredOperationNameIsMissing())
+
+		return report
+	}
+
+	if operationName == "" && numOfOperations == 1 {
+		operationName = operation.OperationDefinitionNameString(0)
+	}
+
+	if !operation.OperationNameExists(operationName) {
+		report.AddExternalError(operationreport.ErrOperationWithProvidedOperationNameNotFound(operationName))
+
+		return report
+	}
+
+	gqlRequest.OperationName = operationName
 
 	return nil
 }
